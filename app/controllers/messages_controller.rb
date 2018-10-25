5907b1fa756d806c580368b0fa1052e4a8b362b5
miclis <mlgt007@gmail.com>
2018-10-25 03:14:42 +0200

    New, create, edit, update, message_params added for messages. Messages views slightly changed.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,4 +13,36 @@ class MessagesController < ApplicationController
     redirect_to messages_path
   end
 
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to @message
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    if @message.update(message_params)
+      redirect_to @message
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :author)
+  end
+
 end
